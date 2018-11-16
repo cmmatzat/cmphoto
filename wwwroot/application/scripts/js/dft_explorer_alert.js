943d8779c30e77e701d8feead977a28df2e53628
Corey Matzat <corey@thematzats.com>
2018-11-16 13:35:42 -0600

    Add alert for IE incompatibility
    Adds js to display a message alerting the user of issues with Internet Explorer.

---
+++ wwwroot/application/scripts/js/dft_explorer_alert.js
@@ -0,0 +1,5 @@
+window.addEventListener( "load", function() {
+    if ( !!document.documentMode ) {
+        alert( "It looks like you are using Internet Explorer!\n\nUnfortunately, Internet Explorer is no longer being updated to support modern web features. As a result, many features of this website will not work properly on Internet Explorer.\n\nFor the best experience, please use your choice of modern browser, such as Microsoft Edge, Google Chrome, Mozilla Firefox, or Safari, to view Corey Matzat Photography.\n\nSorry for the inconvenience!" );
+    }
+});
