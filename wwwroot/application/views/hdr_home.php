1076191ba3b465d028ac47d7e7ed88e31095f6c2
Corey Matzat <corey@thematzats.com>
2018-11-20 13:48:39 -0600

    Add text to header
    Add title to home header

--- wwwroot/application/views/hdr_home.php
+++ wwwroot/application/views/hdr_home.php
@@ -3,5 +3,6 @@
 ?>
 
 <header class="hdr-home" data-img="<?php echo IMG_PATH . 'man-climbing-over-tires.jpg'; ?>">
+    <h1>Corey Matzat Photography</h1>
     <img src="<?php echo LOGO_PATH . 'logo_black.png'; ?>"/>
 </header>
